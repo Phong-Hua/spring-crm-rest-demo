adfe3d3c7087a0fc215acc97b7f5ca876dcf1678
admin <admin@admins-imac.gateway>
2022-10-23 21:36:26 +1100

    5th commit - Create endpoint for add new customer

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +35,18 @@ public class CustomerRestController {
 		if (theCustomer == null) {
 			throw new CustomerNotFoundException("Customer id not found - " + customerId);
 		}
+		return theCustomer;
+	}
+	
+	// add mapping for POST /customers - add new customer
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		
+		// in case they pass the id
+		theCustomer.setId(0);	// we use setId(0) because in the backend, DAO use Hibernate method session.saveOrUpdate(...), and 0 means empty. 
+								//	When the id is empty, then Hibernate will INSERT new customer
+		customerService.saveCustomer(theCustomer);
+		
 		return theCustomer;
 	}
 }
